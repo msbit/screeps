ece32402b06ab00014fce841ceb5da153c341ba9
Tom Sullivan <tom@msbit.com.au>
2016-08-29 15:48:36 +1000

    Add some DN3D quotes

--- role.js
+++ role.js
@@ -1,6 +1,8 @@
 var role_names = ['harvester', 'upgrader'];
 var role_defs = {};
 
+var quotes = require('quotes');
+
 role_names.forEach(function(role_name) {
   role_defs[role_name] = require('role.' + role_name);
 });
@@ -10,5 +12,8 @@ module.exports.tick = function() {
     return Game.creeps[name];
   }).forEach(function(creep) {
     role_defs[creep.memory.role].tick(creep);
+    if(Math.random() > 0.9) {
+      creep.say(quotes[Math.floor(Math.random() * quotes.length)]);
+    }
   });
 };

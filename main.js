0a834dd87215e50154505a3aa59e7abd49aac393
Tom Sullivan <tom@msbit.com.au>
2016-08-29 16:23:15 +1000

    Test out creating stuff

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 Memory.config = require('config');
 var role = require('role');
 var memory = require('memory');
+var room_name = Object.keys(Game.rooms)[0];
+
+var MAX_X = 50;
+var MAX_Y = 50;
 
 var tick = function () {
   memory.tick();
@@ -18,6 +22,13 @@ var tick = function () {
   });
 
   role.tick();
+
+  if(Game.spawns['Spawn1'].energy === Game.spawns['Spawn1'].energyCapacity) {
+    //make somewhere to store the addition
+    var x = Math.floor(Math.random() * MAX_X);
+    var y = Math.floor(Math.random() * MAX_Y);
+    Game.rooms[room_name].createConstructionSite(x, y, STRUCTURE_ROAD);
+  }
 };
 
 module.exports.loop = tick;

3fa2fba793cc365f29c135ca2100c29017cd1b7d
Tom Sullivan <tom@msbit.com.au>
2016-08-29 15:30:59 +1000

    Add config

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  harvester_count: 2,
+  upgrader_count: 1
+};
--- main.js
+++ main.js
@@ -1,8 +1,6 @@
+var config = require('config');
 var role = require('role');
 var memory = require('memory');
-
-var HARVESTER_COUNT = 2;
-var UPGRADER_COUNT = 1;
 
 var tick = function () {
   memory.tick();
@@ -11,7 +9,7 @@ var tick = function () {
     return creep.memory.role === 'harvester';
   });
 
-  if(harvesters.length < HARVESTER_COUNT) {
+  if(harvesters.length < config.harvester_count) {
     Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {
       role: 'harvester'
     });
@@ -21,7 +19,7 @@ var tick = function () {
     return creep.memory.role === 'upgrader';
   });
 
-  if(upgraders.length < UPGRADER_COUNT) {
+  if(upgraders.length < config.upgrader_count) {
     Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {
       role: 'upgrader'
     });

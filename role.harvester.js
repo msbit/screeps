ee255158421b8e80becc05fc1843c6885a4ceca5
Tom Sullivan <tom@msbit.com.au>
2016-08-29 17:09:47 +1000

    Also update harvester target selection

--- role.harvester.js
+++ role.harvester.js
@@ -1,9 +1,8 @@
+var util = require('util');
+
 module.exports.tick = function(creep) {
   if(creep.carry.energy < creep.carryCapacity) {
-    var sources = creep.room.find(FIND_SOURCES);
-    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-      creep.moveTo(sources[0]);
-    }
+    util.go_to_nearest(creep, FIND_SOURCES);
   }
   else {
     if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
